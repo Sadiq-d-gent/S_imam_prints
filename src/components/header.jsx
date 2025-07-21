312d4c0ad703d56aa8fea8e8b40ac05e91decc56
Sadiq Ahmad <138960197+Sadiq-d-gent@users.noreply.github.com>
2025-07-21 07:40:17 +0100

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,7 +10,7 @@ export const Header = (props) => {
               <div className="col-md-8 col-md-offset-2 intro-text">
                 <h1>
                   {props.data ? props.data.title : "Loading"}
-                  <span></span>
+                  <span>prints</span>
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
                 <a
